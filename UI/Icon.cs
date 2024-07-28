deede9f3f0688266f463edbcdd3fbbd0bbb77b17
lbroglio <lbroglio@iastate.edu>
2024-07-28 12:52:45 -0500

    Implemented Icon expansion logic

--- UI/Icon.cs
+++ UI/Icon.cs
@@ -11,6 +11,7 @@ namespace PolyFlow.UI
 {
     internal abstract class Icon : DrawableGameComponent
     {
+        private static readonly float BUTTON_GAP = 10;
 
         /// <summary>
         /// Images associated with this UI icon. The texture in index 0 will be displayed by default. The others will be shown when this icon
@@ -33,12 +34,22 @@ namespace PolyFlow.UI
         private bool _closing = false;
 
         /// <summary>
+        /// The x positon of the top left corner of this Icon on the screen
+        /// </summary>
+        private float _xPos = 0;
+
+        /// <summary>
+        /// The y positon of the top left corner of this Icon on the screen
+        /// </summary>
+        private float _yPos = 0;
+
+        /// <summary>
         /// The length of this icon
         /// </summary>
         private float _length = 100;
 
         /// <summary>
-        /// The height of an indidual clickable button under this icon
+        /// The height of an individual clickable button under this icon
         /// </summary>
         private float _height = 100;
 
@@ -47,6 +58,15 @@ namespace PolyFlow.UI
         /// </summary>
         private float _totalHeight;
 
+        /// <summary>
+        /// The current height this Icon is currently expanded to
+        /// </summary>
+        private float _currentHeight;
+
+        /// <summary>
+        /// The height of this Icon when it is completely unexpanded
+        /// </summary>
+        private float _baseHeight;
 
         /// <summary>
         /// The speed this icon opens and closes 
@@ -54,9 +74,14 @@ namespace PolyFlow.UI
         private float _expandSpeed = 20;
 
 
+
         public Icon(Game game, string[] iconFiles) : base(game)
         {
             Array.Copy(iconFiles, _iconFiles, _icons.Length);
+            _totalHeight = _icons.Length * (_height + ( 2 * BUTTON_GAP)) ;
+            _baseHeight = (_height + (BUTTON_GAP * 2));
+            _currentHeight = _baseHeight;
+
         }
 
 
@@ -71,13 +96,66 @@ namespace PolyFlow.UI
 
         public override void Update(GameTime gameTime)
         {
-
+            // Move logic
 
             // If this icon is currently opening
             if (_opening)
             {
+                if(_currentHeight < _totalHeight)
+                {
+                    if(_currentHeight + _expandSpeed > _totalHeight)
+                    {
+                        _currentHeight += (_totalHeight - _currentHeight);
+                    }
+                    else
+                    {
+                        _currentHeight += _expandSpeed;
+                    }
+                    
+                }
+                else
+                {
+                    _opening = false;
+                }
+            }
+
+            //If this Icon is currently closing
+            if(_closing)
+            {
+                if (_currentHeight > _baseHeight)
+                {
+                    if (_currentHeight - _expandSpeed < _totalHeight)
+                    {
+                        _currentHeight -= (_currentHeight - _baseHeight);
+                    }
+                    else
+                    {
+                        _currentHeight -= _expandSpeed;
+                    }
+                }
+                else
+                {
+                    _closing = false;
+                }
             }
             
+            // Start opening or closing
+            MouseState mouseState = Mouse.GetState();
+
+            if(mouseState.X > _xPos && mouseState.X < (_xPos + _length) && mouseState.Y > (_yPos) && mouseState.Y < (_yPos + _currentHeight))
+            {
+                if(_currentHeight < _totalHeight)
+                {
+                    _opening = true;
+                }
+            }
+            else
+            {
+                if(_currentHeight < _baseHeight)
+                {
+                    _closing = true;
+                }
+            }
 
             // TODO: Add your update logic here
 
